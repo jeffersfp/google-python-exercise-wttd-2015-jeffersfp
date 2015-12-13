b36648a2a148aa12d7fb957e59c7894a2dc61962
Jefferson Pires <jeffersfp@gmail.com>
2015-12-13 20:50:33 -0200

    exercises solved

--- basic/list2.py
+++ basic/list2.py
@@ -15,7 +15,11 @@
 # modify the passed in list.
 def remove_adjacent(nums):
     # +++your code here+++
-    return
+    lista = []
+    for n in nums:
+        if len(lista) == 0 or n != lista[-1]:
+            lista.append(n)
+    return lista
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -24,16 +28,46 @@ def remove_adjacent(nums):
 # pass of both lists.
 def linear_merge(list1, list2):
     # +++your code here+++
-    return
+    result = []
+    # Look at the two lists so long as both are non-empty.
+    # Take whichever first element is smaller.
+    while len(list1) and len(list2):
+        if list1[0] < list2[0]:
+            result.append(list1.pop(0))
+        else:
+            result.append(list2.pop(0))
+
+    # Now tack on what's left
+    result.extend(list1)
+    result.extend(list2)
+    return result
 
 
-# Note: the solution above is kind of cute, but unforunately list.pop(0)
+# Note: the solution above is kind of cute, but unfortunately list.pop(0)
 # is not constant time with the standard python list implementation, so
 # the above is not strictly linear time.
 # An alternate approach uses pop(-1) to remove the endmost elements
 # from each list, building a solution list which is backwards.
 # Then use reversed() to put the result back in the correct order. That
 # solution works in linear time, but is more ugly.
+
+
+def reversed_linear_merge(list1, list2):
+    # +++your code here+++
+    result = []
+    # Look at the two lists so long as both are non-empty.
+    # Take whichever last element is bigger.
+    while len(list1) and len(list2):
+        if list1[-1] > list2[-1]:
+            result.append(list1.pop(-1))
+        else:
+            result.append(list2.pop(-1))
+
+    # Now tack on what's left
+    result.extend(reversed(list1))
+    result.extend(reversed(list2))
+    result.sort()
+    return result
 
 
 # Simple provided test() function used in main() to print
@@ -60,6 +94,15 @@ def main():
     test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
          ['aa', 'bb', 'cc', 'xx', 'zz'])
     test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+         ['aa', 'aa', 'aa', 'bb', 'bb'])
+
+    print()
+    print('reversed_linear_merge')
+    test(reversed_linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(reversed_linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(reversed_linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
          ['aa', 'aa', 'aa', 'bb', 'bb'])
 
 
